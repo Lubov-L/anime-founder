f153b880455edbd3e710daafa45aa1126ac6e3d1
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-07-11 23:37:03 +0300

    работа с Jikan API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Sidebar from "./components/main/Sidebar";
 
 function App() {
     const [animeList, SetAnimeList] = useState([]);
     const [topAnime, SetTopAnime] = useState([]);
     const [search, SetSearch] = useState("");
+
+    const GetTopAnime = async () => {
+        const temp = await fetch('https://api.jikan.moe/v4/anime/1/relations')
+            .then(res => res.json());
+
+        SetTopAnime(temp.top);
+    }
+
+    useEffect(() => {
+        GetTopAnime();
+
+    }, []);
+
+    console.log(topAnime);
 
     return (
         <div className="App">
