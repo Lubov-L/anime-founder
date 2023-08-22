a9a3719b9f5e7bff6014c694df047f7ff9388215
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-08-22 23:19:26 +0300

    styles editing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
-import './App.css';
+import './app.css';
 import {useEffect, useState} from "react";
 import Sidebar from "./components/main/Sidebar";
 
